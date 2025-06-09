358638cfd1951475b89d1a41361000e5749d2336
GregoryVDev <arexo@hotmail.fr>
2025-06-09 18:56:26 +0200

    Displayed the li and link in Navigation component file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import { Home } from "./pages/Home";
 import { About } from "./pages/About";
+import { Navigation } from "./components/Navigation";
 
 export const App = () => {
   return (
     <div className="app-container">
+      <Navigation />
       <Home />
       <About />
     </div>
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,16 @@
+export const Navigation = () => {
+  return (
+    <div>
+      <div className="navigation">
+        <ul>
+          <li>
+            <a href="/accueil">Accueil</a>
+          </li>
+          <li>
+            <a href="/about">A propos</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
