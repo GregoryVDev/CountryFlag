25e91f37bbdc7f2eee0a0a628c054fa639a9081c
GregoryVDev <arexo@hotmail.fr>
2025-06-09 18:26:27 +0200

    Display the h1 from Home and About files with the paragraph

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import { Home } from "./pages/Home";
+import { About } from "./pages/About";
 
 export const App = () => {
   return (
     <div className="app-container">
       <Home />
+      <About />
     </div>
   );
 };
