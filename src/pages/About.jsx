843c61f5edc61ee3e74d43f6e901fb408010979b
GregoryVDev <arexo@hotmail.fr>
2025-06-09 18:11:48 +0200

    Created the About file with the component About with a h1 and two paragraph

---
+++ src/pages/About.jsx
@@ -0,0 +1,30 @@
+export const About = () => {
+  return (
+    <div>
+      <h1>A propos</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam et at
+        blanditiis totam veritatis praesentium numquam minus, rerum, ullam
+        provident soluta animi illo. Rem quidem tenetur iusto, voluptates
+        provident ex sunt explicabo molestias natus adipisci commodi unde
+        consequatur perferendis hic? Sed tempora eaque et minima porro.
+        Perferendis quas dolorum optio ipsa doloremque eaque officia ducimus
+        facilis laudantium dolores cupiditate debitis, aliquid repudiandae neque
+        vero, eveniet quidem accusantium! Tempore, magnam fuga, quisquam saepe
+        nulla molestias debitis eaque quae quidem atque ipsam obcaecati, sed
+        consequuntur? Sapiente ex officiis saepe ipsa natus neque voluptatem
+        numquam quidem, facilis nulla. Soluta distinctio sed eaque sequi!
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere dolor
+        nesciunt doloribus deleniti, voluptatem optio itaque dolorem delectus.
+        Quod officia voluptates corrupti. Accusamus doloremque quos nisi tenetur
+        quae ipsa culpa provident? Tenetur vero recusandae, quas, sit vitae
+        voluptatibus hic numquam culpa, at laborum doloribus aspernatur. Vitae
+        similique in voluptates hic?
+      </p>
+    </div>
+  );
+};
