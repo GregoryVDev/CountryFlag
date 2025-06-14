c2de113d9bc952330ceebfb43be1191ad9ba1042
GregoryVDev <arexo@hotmail.fr>
2025-06-14 16:27:38 +0200

    Added the border-bottom for the nav active

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,14 +1,24 @@
+import { NavLink } from "react-router-dom";
+
 export const Navigation = () => {
   return (
     <div>
       <div className="navigation">
         <ul>
-          <li>
-            <a href="/accueil">Accueil</a>
-          </li>
-          <li>
-            <a href="/about">A propos</a>
-          </li>
+          <NavLink
+            to="/"
+            className={(nav) => (nav.isActive ? "nav-active" : "")}
+          >
+            <li>Accueil</li>
+          </NavLink>
+          <NavLink
+            to="/about"
+            className={(nav) => (nav.isActive ? "nav-active" : "")}
+          >
+            <li>
+              <a href="/about">A propos</a>
+            </li>
+          </NavLink>
         </ul>
       </div>
     </div>
