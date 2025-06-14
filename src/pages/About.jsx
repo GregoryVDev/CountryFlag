d6c1cbba50e19ac0633801e17076fdfbc2a05bdd
GregoryVDev <arexo@hotmail.fr>
2025-06-14 02:02:23 +0200

    Added the image for Logo file and added the component Logo in Home file

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,5 @@
 import { Navigation } from "../components/Navigation";
+import { Logo } from "../components/Logo";
 
 export const About = () => {
   return (
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import { Navigation } from "../components/Navigation";
+import { Logo } from "../components/Logo";
 
 export const Home = () => {
   return (
     <div>
+      <Logo />
       <Navigation />
       <h1>Accueil</h1>
     </div>
