c915c6fe8e7ff7733d703310583b07f75b31582c
GregoryVDev <arexo@hotmail.fr>
2025-06-16 17:11:36 +0200

    Added the h2 with the className "infos"

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,11 @@
 export const Card = () => {
   return (
     <div>
-      <li className="card"></li>
+      <li className="card">
+        <div className="infos">
+          <h2>Nom du pays</h2>
+        </div>
+      </li>
     </div>
   );
 };
