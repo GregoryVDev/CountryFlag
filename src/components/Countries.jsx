cab81af621e4d803a29724b8ff900e8d5b6995fe
GregoryVDev <arexo@hotmail.fr>
2025-06-16 16:27:16 +0200

    Edited the console.log by setData

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,12 +1,14 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export const Countries = () => {
+  // setData permet de modifier les données stockés dans data
+  const [data, setData] = useState([]);
   // Faire un useEffect pour récupérer les données dans un API
   useEffect(() => {
     axios
       .get("https://restcountries.com/v3.1/all?fields=name,capital,currencies")
-      .then((res) => console.log(res.data));
+      .then((res) => setData(res.data));
   }, []);
 
   return (
