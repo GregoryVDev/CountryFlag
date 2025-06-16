6adaf4b9927a539e38c312dcdcb51978886134fe
GregoryVDev <arexo@hotmail.fr>
2025-06-16 15:50:31 +0200

    Added useEffect for recovering the data from an API

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -1,4 +1,10 @@
+import axios from "axios";
+import { useEffect } from "react";
+
 export const Countries = () => {
+  // Faire un useEffect pour récupérer les données dans un API
+  useEffect(() => {}, []);
+
   return (
     <div className="countries">
       <h1>COUNTRIES</h1>
