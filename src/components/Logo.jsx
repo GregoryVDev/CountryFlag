65b320f8dbfaa427862a07488bbb5e1ddda5fce4
GregoryVDev <arexo@hotmail.fr>
2025-06-16 15:25:39 +0200

    Added image logo,  display flex for the logo and edited the color for the link

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,7 +2,7 @@ export const Logo = () => {
   return (
     <div className="logo">
       {/* Les images importées depuis la balise IMG sont accessible dans "public" */}
-      <img src="" alt="Logo react" />
+      <img src="/public/logo192.png" alt="Logo react" />
       <h3>React World</h3>
     </div>
   );
