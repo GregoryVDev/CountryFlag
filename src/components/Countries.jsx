0293a2a57c573b1e3067c6402505411e87c60e7a
GregoryVDev <arexo@hotmail.fr>
2025-06-16 15:40:59 +0200

    Removed the h1 in home file and called the Countries component

---
+++ src/components/Countries.jsx
@@ -0,0 +1,7 @@
+export const Countries = () => {
+  return (
+    <div>
+      <h1>COUNTRIES</h1>
+    </div>
+  );
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,13 @@
 import { Navigation } from "../components/Navigation";
 import { Logo } from "../components/Logo";
+import { Countries } from "../components/Countries";
 
 export const Home = () => {
   return (
     <div>
       <Logo />
       <Navigation />
-      <h1>Accueil</h1>
+      <Countries />
     </div>
   );
 };
