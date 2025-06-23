64b253a431aafce8d2089d62c16103f1cd19686d
GregoryVDev <arexo@hotmail.fr>
2025-06-23 18:37:46 +0200

    Added handleSubmit function to prevent the form from being reloaded

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -2,17 +2,21 @@ import { Navigation } from "../components/Navigation";
 import { Logo } from "../components/Logo";
 
 export const Blog = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
     <div className="blog-container">
       <Logo />
       <Navigation />
       <h1>Blog</h1>
 
-      <form>
+      <form onSubmit={(e) => handleSubmit(e)}>
         <input type="text" placeholder="Nom" />
         <textarea placeholder="Message"></textarea>
         <input type="submit" value="Envoyer" />
       </form>
+      <ul></ul>
     </div>
   );
 };
