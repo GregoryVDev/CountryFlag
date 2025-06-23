b54f385297f4635339b3ec547bd4cd7ea47e4768
GregoryVDev <arexo@hotmail.fr>
2025-06-23 17:45:48 +0200

    Added npm i sass for the package.json file. Added Blog.jsx file and blog.css style.  Added for the navLink the blog file

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./styles/App.css";
 import "./styles/index.css";
 import "./styles/logo.css";
+import "./styles/blog.css";
 import { Home } from "./pages/Home";
 import { About } from "./pages/About";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Blog } from "./pages/Blog";
 
 export const App = () => {
   return (
@@ -13,6 +15,7 @@ export const App = () => {
         <Route path="/about" element={<About />} />
         {/* path="*" fonctionne si jamais l'url ne correspond à rien de déclaré au dessus */}
         <Route path="*" element={<Home />} />
+        <Route path="*" element={<Blog />} />
       </Routes>
     </BrowserRouter>
   );
