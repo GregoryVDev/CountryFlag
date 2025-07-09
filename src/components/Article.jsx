2c584fc27927911362ae3eb182296dd83b692fcf
GregoryVDev <arexo@hotmail.fr>
2025-07-09 12:47:05 +0200

    Added a newDate variable using the French time in France. displayed numerically and the month written in long.

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,10 +1,21 @@
 export const Article = ({ article }) => {
+  const dateFormater = (date) => {
+    let newDate = new Date(date).toLocaleDateString("fr-FR", {
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+      seconde: "numeric",
+    });
+    return newDate;
+  };
   return (
     <div>
       <div className="article">
         <div className="card-header">
           <h3>{article.author}</h3>
-          <em>Posté le {article.date}</em>
+          <em>Posté le {dateFormater(article.date)}</em>
         </div>
         <p>{article.content}</p>
         <div className="btn-container">
