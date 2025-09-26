d6bd9a564bbe3938e6e38905921a33665f1e9da9
GregoryVDev <arexo@hotmail.fr>
2025-09-26 21:12:59 +0200

    Added the project in FileZilla with the edit

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,8 +1,9 @@
+import reactLogo from "/public/assets/react.svg";
+
 export const Logo = () => {
   return (
     <div className="logo">
-      {/* Les images importées depuis la balise IMG sont accessible dans "public" */}
-      <img src="/public/logo192.png" alt="Logo react" />
+      <img src={reactLogo} alt="Logo react" />
       <h3>React World</h3>
     </div>
   );
